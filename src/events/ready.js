07c2ec1603591111fcc08931e3a07789f162716a
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-30 13:53:52 -0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,8 @@
 const { cacheGuildInvites } = require('../utils/inviteCache');
+const { initDb } = require('../utils/db');
+const { updateLiveLevelLeaderboard, updateLiveRatingLeaderboard } = require('../utils/liveLeaderboard');
+
+const LEVEL_LEADERBOARD_INTERVAL_MS = 60 * 1000; // refresh every 60 seconds
 
 module.exports = {
     name: 'clientReady',
@@ -6,8 +10,17 @@ module.exports = {
     async execute(client) {
         console.log(`Logged in as ${client.user.tag}`);
 
+        await initDb().catch(err => console.error('Failed to initialize database:', err));
+
         for (const guild of client.guilds.cache.values()) {
             await cacheGuildInvites(guild);
         }
+
+        updateLiveRatingLeaderboard(client).catch(() => {});
+        updateLiveLevelLeaderboard(client).catch(() => {});
+
+        setInterval(() => {
+            updateLiveLevelLeaderboard(client).catch(() => {});
+        }, LEVEL_LEADERBOARD_INTERVAL_MS);
     }
 };
