4a2d5167d04b5ac2924225025aaf117a4dcc97fd
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-30 13:50:18 -0500

    Update customCommandsStore.js

--- src/utils/customCommandsStore.js
+++ src/utils/customCommandsStore.js
@@ -1,38 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const { pool } = require('./db');
 
-const filePath = path.join(__dirname, '..', '..', 'customCommands.json');
-
-function loadCommands() {
-    if (!fs.existsSync(filePath)) return {};
-    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+async function addCommand(keyword, response) {
+    await pool.query(
+        `INSERT INTO custom_commands (keyword, response) VALUES ($1, $2)
+         ON CONFLICT (keyword) DO UPDATE SET response = $2`,
+        [keyword.toLowerCase(), response]
+    );
 }
 
-function saveCommands(data) {
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+async function removeCommand(keyword) {
+    const { rowCount } = await pool.query('DELETE FROM custom_commands WHERE keyword = $1', [keyword.toLowerCase()]);
+    return rowCount > 0;
 }
 
-function addCommand(keyword, response) {
-    const data = loadCommands();
-    data[keyword.toLowerCase()] = response;
-    saveCommands(data);
+async function getResponse(keyword) {
+    const { rows } = await pool.query('SELECT response FROM custom_commands WHERE keyword = $1', [keyword.toLowerCase()]);
+    return rows.length ? rows[0].response : null;
 }
 
-function removeCommand(keyword) {
-    const data = loadCommands();
-    if (!(keyword.toLowerCase() in data)) return false;
-    delete data[keyword.toLowerCase()];
-    saveCommands(data);
-    return true;
-}
-
-function getResponse(keyword) {
-    const data = loadCommands();
-    return data[keyword.toLowerCase()] || null;
-}
-
-function listCommands() {
-    return loadCommands();
+async function listCommands() {
+    const { rows } = await pool.query('SELECT keyword, response FROM custom_commands');
+    const result = {};
+    rows.forEach(r => { result[r.keyword] = r.response; });
+    return result;
 }
 
 module.exports = { addCommand, removeCommand, getResponse, listCommands };
