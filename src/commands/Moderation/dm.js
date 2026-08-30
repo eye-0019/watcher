febda79aab42a408956cb401d80b9957d8f5c835
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-30 01:46:43 -0500

    Update dm.js

--- src/commands/Moderation/dm.js
+++ src/commands/Moderation/dm.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { setDmContext } = require('../../utils/dmContext');
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const { setTicket, getChannelForUser } = require('../../utils/dmTicketsStore');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dm')
-        .setDescription('DM a user, their replies get forwarded to your logs channel')
+        .setDescription('DM a user, their replies show up in a private channel here')
         .addUserOption(option =>
             option.setName('target')
                 .setDescription('The user to DM')
@@ -21,10 +21,32 @@ module.exports = {
 
         try {
             await target.send(message);
-            setDmContext(target.id, interaction.guild.id);
-            return interaction.reply({ content: `DM sent to ${target.tag}. Their replies will show up in your logs channel.`, ephemeral: true });
         } catch (err) {
             return interaction.reply({ content: `Could not DM ${target.tag}. They may have DMs closed.`, ephemeral: true });
         }
+
+        let channelId = getChannelForUser(target.id);
+        let channel = channelId ? interaction.guild.channels.cache.get(channelId) : null;
+
+        if (!channel) {
+            const overwrites = [
+                { id: interaction.guild.roles.everyone.id, deny: [PermissionFlagsBits.ViewChannel] }
+            ];
+            const staffRoleId = process.env.STAFF_ROLE_ID;
+            if (staffRoleId) {
+                overwrites.push({ id: staffRoleId, allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages] });
+            }
+
+            channel = await interaction.guild.channels.create({
+                name: `dm-${target.username}`.toLowerCase().replace(/[^a-z0-9-]/g, '').slice(0, 90) || `dm-${target.id}`,
+                type: ChannelType.GuildText,
+                parent: process.env.MODMAIL_CATEGORY_ID || undefined,
+                permissionOverwrites: overwrites
+            }).catch(() => null);
+
+            if (channel) setTicket(target.id, channel.id);
+        }
+
+        return interaction.reply(`DM sent to ${target.tag}.${channel ? ` Their replies will show up in ${channel}.` : ''}`);
     }
 };
