ec3a89b7e0a974b28261c2860297e164837b0452
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-01 18:57:29 -0500

    Update guildMemberRemove.js

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -2,62 +2,74 @@ const { EmbedBuilder } = require('discord.js');
 const { pool } = require('../utils/db');
 
 const LEAVE_MESSAGES = [
-  'Goodbye {user}! Hope to see you again. ❤️',
-  '{user} has left the server.',
-  'See you later, {user}! 👋',
-  '{user} headed out. Take care!',
-  'Goodbye {user}. We will miss you. 😢',
-  '{user} has left the server. Take care!',
-  'Looks like {user} is heading out. 👀',
-  '{user} has left. Goodbye!',
-  'See you around, {user}! 🥲',
-  '{user} is off to somewhere else.'
+    'Goodbye {user}! Hope to see you again. ❤️',
+    '{user} has left the server.',
+    'See you later, {user}! 😌',
+    '{user} headed out. Take care!',
+    'Goodbye {user}. We will miss you. 😢',
+    '{user} has left the server. Take care!',
+    'Looks like {user} is heading out. 👀',
+    '{user} has left. Goodbye!',
+    'See you around, {user}! 🥲',
+    '{user} is off to somewhere else.'
 ];
 
 function getRandomLeaveMessage(user) {
-  const message =
-    LEAVE_MESSAGES[
-      Math.floor(Math.random() * LEAVE_MESSAGES.length)
-    ];
+    const message =
+        LEAVE_MESSAGES[
+            Math.floor(Math.random() * LEAVE_MESSAGES.length)
+        ];
 
-  return message.replace('{user}', user);
+    return message.replace('{user}', user);
 }
 
 module.exports = async function guildMemberRemove(member) {
-  try {
-    const leaveChannelId = process.env.WELCOME_CHANNEL_ID;
+    try {
+        const leaveChannelId = process.env.WELCOME_CHANNEL_ID;
 
-    if (leaveChannelId) {
-      const channel = member.guild.channels.cache.get(leaveChannelId);
+        if (leaveChannelId) {
+            const channel =
+                member.guild.channels.cache.get(
+                    leaveChannelId
+                );
 
-      if (channel && channel.isTextBased()) {
-        const leaveMessage = getRandomLeaveMessage(member.user.tag);
+            if (channel && channel.isTextBased()) {
+                const leaveMessage =
+                    getRandomLeaveMessage(
+                        member.user.tag
+                    );
 
-        const embed = new EmbedBuilder()
-          .setColor(0xed4245)
-          .setDescription(leaveMessage)
-          .setTimestamp();
+                const embed =
+                    new EmbedBuilder()
+                        .setColor(0xed4245)
+                        .setDescription(
+                            leaveMessage
+                        )
+                        .setTimestamp();
 
-        await channel.send({
-          embeds: [embed]
-        });
-      }
+                await channel.send({
+                    embeds: [embed]
+                });
+            }
+        }
+
+        await pool.query(
+            `
+            UPDATE member_joins
+            SET left_at = NOW()
+            WHERE user_id = $1
+              AND guild_id = $2
+              AND left_at IS NULL
+            `,
+            [
+                member.id,
+                member.guild.id
+            ]
+        );
+    } catch (error) {
+        console.error(
+            `[Leave] Error handling ${member.user.tag} leaving ${member.guild.name}:`,
+            error
+        );
     }
-
-    await pool.query(
-      `
-      UPDATE member_joins
-      SET left_at = NOW()
-      WHERE user_id = $1
-        AND guild_id = $2
-        AND left_at IS NULL
-      `,
-      [member.id, member.guild.id]
-    );
-  } catch (error) {
-    console.error(
-      `[Leave] Error handling ${member.user.tag} leaving ${member.guild.name}:`,
-      error
-    );
-  }
 };
