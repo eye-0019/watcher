44c9da7ae013190fc30f9a0b64e0f335fb26fff8
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 13:24:40 -0500

    Refactor logging channels structure for clarity

--- src/logger/channels.js
+++ src/logger/channels.js
@@ -1,4 +1,5 @@
 module.exports = {
+
     moderation: {
         ban: process.env.BAN_LOG_CHANNEL,
         kick: process.env.KICK_LOG_CHANNEL,
@@ -6,28 +7,46 @@ module.exports = {
         timeout: process.env.TIMEOUT_LOG_CHANNEL
     },
 
+
     members: {
         join: process.env.JOIN_LOG_CHANNEL,
         leave: process.env.LEAVE_LOG_CHANNEL,
         update: process.env.MEMBER_UPDATE_LOG_CHANNEL
     },
 
+
     messages: {
         delete: process.env.MESSAGE_DELETE_LOG_CHANNEL,
         edit: process.env.MESSAGE_EDIT_LOG_CHANNEL
     },
 
-    server: {
-        roles: process.env.ROLE_LOG_CHANNEL,
-        channels: process.env.CHANNEL_LOG_CHANNEL,
-        invites: process.env.INVITE_LOG_CHANNEL
+
+    roles: {
+        create: process.env.ROLE_LOG_CHANNEL,
+        delete: process.env.ROLE_LOG_CHANNEL
     },
+
+
+    channels: {
+        create: process.env.CHANNEL_LOG_CHANNEL,
+        delete: process.env.CHANNEL_LOG_CHANNEL
+    },
+
+
+    invites: {
+        create: process.env.INVITE_LOG_CHANNEL
+    },
+
 
     voice: {
         update: process.env.VOICE_LOG_CHANNEL
     },
 
+
     bot: {
         errors: process.env.ERROR_LOG_CHANNEL
-    }
+    },
+
+
+    system: process.env.ERROR_LOG_CHANNEL
 };
