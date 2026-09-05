d273eee36e42f2028bababd97d5f6102f218ba53
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 12:48:46 -0500

    Enhance inviteDelete event logging functionality

--- src/events/inviteDelete.js
+++ src/events/inviteDelete.js
@@ -1,8 +1,70 @@
-const { cacheGuildInvites } = require('../utils/inviteCache');
+const { createLog } = require("../logger/logger");
+const channels = require("../logger/channels");
+
 
 module.exports = {
-    name: 'inviteDelete',
+    name: "inviteDelete",
+
     async execute(invite) {
-        await cacheGuildInvites(invite.guild);
+
+        if (!invite.guild) return;
+
+
+        await createLog(invite.guild, {
+
+            type: "server",
+
+            action: "Invite Deleted",
+
+
+            channel: invite.channel
+                ? invite.channel.id
+                : null,
+
+
+            description:
+                `Invite **${invite.code}** was deleted.`,
+
+
+            severity: "normal",
+
+
+            logChannel: channels.server.invites,
+
+
+            metadata: {
+
+                code: invite.code,
+
+                channelId:
+                    invite.channel
+                        ? invite.channel.id
+                        : null
+
+            },
+
+
+            color: 0xff9900,
+
+
+            fields: [
+
+                {
+                    name: "Invite Code",
+                    value: invite.code
+                },
+
+                {
+                    name: "Channel",
+                    value:
+                        invite.channel
+                            ? `<#${invite.channel.id}>`
+                            : "Unknown"
+                }
+
+            ]
+
+        });
+
     }
 };
