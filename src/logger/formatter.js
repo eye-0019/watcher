154a1a5ade07f769869ba165dcb51515745aefd1
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 12:39:15 -0500

    Update formatter.js

--- src/logger/formatter.js
+++ src/logger/formatter.js
@@ -1,1 +1,40 @@
 
+const { EmbedBuilder } = require("discord.js");
+
+
+function createLogEmbed({
+    action,
+    description,
+    type,
+    severity = "normal",
+    fields = [],
+    color = 0xffffff
+}) {
+
+    const embed = new EmbedBuilder()
+        .setTitle(action)
+        .setDescription(description)
+        .addFields(
+            {
+                name: "Event Type",
+                value: type || "unknown",
+                inline: true
+            },
+            {
+                name: "Severity",
+                value: severity,
+                inline: true
+            },
+            ...fields
+        )
+        .setColor(color)
+        .setTimestamp();
+
+
+    return embed;
+}
+
+
+module.exports = {
+    createLogEmbed
+};
