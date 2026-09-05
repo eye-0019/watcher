273163d1d11c9654119b7c43f2775d027a31f65f
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 12:58:23 -0500

    Refactor addrole command to use user option

--- src/commands/Moderation/addrole.js
+++ src/commands/Moderation/addrole.js
@@ -1,35 +1,131 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { createLog } = require("../../logger/logger");
+const channels = require("../../logger/channels");
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('addrole')
-        .setDescription('Add a role to a member')
+
+        .setName("addrole")
+        .setDescription("Add a role to a member")
+
+        .setDefaultMemberPermissions(
+            PermissionFlagsBits.ManageRoles
+        )
+
         .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The member to give the role to')
-                .setRequired(true))
+            option
+                .setName("user")
+                .setDescription("User to give role to")
+                .setRequired(true)
+        )
+
         .addRoleOption(option =>
-            option.setName('role')
-                .setDescription('The role to add')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('Reason for adding this role')
-                .setRequired(false))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+            option
+                .setName("role")
+                .setDescription("Role to add")
+                .setRequired(true)
+        ),
+
+
+
     async execute(interaction) {
-        const target = interaction.options.getMember('target');
-        const role = interaction.options.getRole('role');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
 
-        if (!target) {
-            return interaction.reply({ content: 'Could not find that member.', ephemeral: true });
-        }
-        if (target.roles.cache.has(role.id)) {
-            return interaction.reply({ content: `${target.user.tag} already has that role.`, ephemeral: true });
+        const user =
+            interaction.options.getUser("user");
+
+
+        const role =
+            interaction.options.getRole("role");
+
+
+
+        const member =
+            await interaction.guild.members
+                .fetch(user.id)
+                .catch(() => null);
+
+
+
+        if (!member) {
+            return interaction.reply({
+                content: "User not found.",
+                ephemeral: true
+            });
         }
 
-        await target.roles.add(role, reason);
-        return interaction.reply(`Added ${role} to ${target.user.tag}. Reason: ${reason}`);
+
+
+        await member.roles.add(role);
+
+
+
+        await interaction.reply({
+            content:
+                `Added ${role.name} to ${user.tag}.`,
+            ephemeral: true
+        });
+
+
+
+        await createLog(interaction.guild, {
+
+            type: "moderation",
+
+            action: "Role Added",
+
+
+            target: user.id,
+
+
+            executor:
+                interaction.user.id,
+
+
+            description:
+                `${role.name} was added to ${user.tag}.`,
+
+
+            severity: "normal",
+
+
+            logChannel:
+                channels.server.roles,
+
+
+            metadata: {
+
+                userId: user.id,
+
+                username: user.tag,
+
+                roleId: role.id,
+
+                roleName: role.name
+
+            },
+
+
+            color: 0x57f287,
+
+
+            fields: [
+
+                {
+                    name: "Moderator",
+                    value:
+                        `${interaction.user.tag} (${interaction.user.id})`
+                },
+
+                {
+                    name: "Role",
+                    value:
+                        `${role.name} (${role.id})`
+                }
+
+            ]
+
+        });
+
     }
 };
