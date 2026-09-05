9736d6587f07df4ffd85434d28b528af7bafcaa5
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 12:56:39 -0500

    Update purge.js

--- src/commands/Moderation/purge.js
+++ src/commands/Moderation/purge.js
@@ -1,42 +1,115 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { logAction } = require('../../utils/logAction');
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { createLog } = require("../../logger/logger");
+const channels = require("../../logger/channels");
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('purge')
-        .setDescription('Delete a number of messages from this channel')
+
+        .setName("purge")
+        .setDescription("Delete multiple messages")
+
+        .setDefaultMemberPermissions(
+            PermissionFlagsBits.ManageMessages
+        )
+
         .addIntegerOption(option =>
-            option.setName('amount')
-                .setDescription('How many messages to delete')
-                .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+            option
+                .setName("amount")
+                .setDescription("Number of messages to delete")
+                .setRequired(true)
+        ),
+
+
 
     async execute(interaction) {
-        let amount = interaction.options.getInteger('amount');
-        if (amount < 1) {
-            return interaction.reply({ content: 'Amount must be at least 1.', ephemeral: true });
+
+        const amount =
+            interaction.options.getInteger("amount");
+
+
+
+        if (amount < 1 || amount > 100) {
+            return interaction.reply({
+                content: "Amount must be between 1 and 100.",
+                ephemeral: true
+            });
         }
 
-        await interaction.deferReply({ ephemeral: true });
 
-        let deletedTotal = 0;
-        while (amount > 0) {
-            const chunk = Math.min(amount, 100);
-            const deleted = await interaction.channel.bulkDelete(chunk, true).catch(() => null);
-            if (!deleted || deleted.size === 0) break;
-            deletedTotal += deleted.size;
-            amount -= deleted.size;
-            if (deleted.size < chunk) break;
-        }
 
-        await logAction(interaction.guild, {
-            action: 'Messages Purged',
-            moderator: interaction.user,
-            target: interaction.user,
-            reason: `${deletedTotal} messages deleted in ${interaction.channel}`,
-            color: 0x95A5A6
+        const deleted =
+            await interaction.channel.bulkDelete(
+                amount,
+                true
+            );
+
+
+
+        await interaction.reply({
+            content:
+                `Deleted ${deleted.size} messages.`,
+            ephemeral: true
         });
 
-        return interaction.editReply(`Deleted ${deletedTotal} message(s).`);
+
+
+        await createLog(interaction.guild, {
+
+            type: "moderation",
+
+            action: "Messages Purged",
+
+
+            executor:
+                interaction.user.id,
+
+
+            channel:
+                interaction.channel.id,
+
+
+            description:
+                `${interaction.user.tag} deleted ${deleted.size} messages in <#${interaction.channel.id}>.`,
+
+
+            severity: "medium",
+
+
+            logChannel:
+                channels.messages.delete,
+
+
+            metadata: {
+
+                amount: deleted.size,
+
+                channelId:
+                    interaction.channel.id
+
+            },
+
+
+            color: 0xff9900,
+
+
+            fields: [
+
+                {
+                    name: "Moderator",
+                    value:
+                        `${interaction.user.tag} (${interaction.user.id})`
+                },
+
+                {
+                    name: "Channel",
+                    value:
+                        `<#${interaction.channel.id}>`
+                }
+
+            ]
+
+        });
+
     }
 };
