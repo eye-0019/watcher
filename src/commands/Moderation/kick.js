5eda146e8a6f6e87dc10fd4cf9ee961eb064939c
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 12:50:56 -0500

    Update kick.js

--- src/commands/Moderation/kick.js
+++ src/commands/Moderation/kick.js
@@ -1,40 +1,107 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { logAction } = require('../../utils/logAction');
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { createLog } = require("../../logger/logger");
+const channels = require("../../logger/channels");
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('kick')
-        .setDescription('Kick a member')
+        .setName("kick")
+        .setDescription("Kick a member")
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
         .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The member to kick')
-                .setRequired(true))
+            option
+                .setName("user")
+                .setDescription("User to kick")
+                .setRequired(true)
+        )
         .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('Reason for kicking')
-                .setRequired(false))
-        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+            option
+                .setName("reason")
+                .setDescription("Reason for kick")
+                .setRequired(false)
+        ),
+
 
     async execute(interaction) {
-        const target = interaction.options.getMember('target');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
 
-        if (!target) {
-            return interaction.reply({ content: 'Could not find that member.', ephemeral: true });
-        }
-        if (!target.kickable) {
-            return interaction.reply({ content: 'I cannot kick that member.', ephemeral: true });
+        const user = interaction.options.getUser("user");
+        const reason =
+            interaction.options.getString("reason")
+            || "No reason provided";
+
+
+        const member = await interaction.guild.members.fetch(user.id)
+            .catch(() => null);
+
+
+        if (!member) {
+            return interaction.reply({
+                content: "User not found.",
+                ephemeral: true
+            });
         }
 
-        await target.kick(reason);
-        await logAction(interaction.guild, {
-            action: 'Member Kicked',
-            moderator: interaction.user,
-            target: target.user,
-            reason,
-            color: 0x95A5A6
+
+        await member.kick(reason);
+
+
+
+        await interaction.reply({
+            content:
+                `${user.tag} has been kicked.`,
+            ephemeral: true
         });
 
-        return interaction.reply(`Kicked ${target.user.tag}. Reason: ${reason}`);
+
+
+        await createLog(interaction.guild, {
+
+            type: "moderation",
+
+            action: "Member Kicked",
+
+
+            target: user.id,
+
+
+            executor: interaction.user.id,
+
+
+            description:
+                `${user.tag} was kicked.\nReason: ${reason}`,
+
+
+            severity: "high",
+
+
+            logChannel: channels.moderation.kick,
+
+
+            metadata: {
+
+                username: user.tag,
+
+                userId: user.id,
+
+                reason
+
+            },
+
+
+            color: 0xff0000,
+
+
+            fields: [
+
+                {
+                    name: "Moderator",
+                    value:
+                        `${interaction.user.tag} (${interaction.user.id})`
+                }
+
+            ]
+
+        });
+
     }
 };
