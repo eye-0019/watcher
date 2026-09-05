c2557d04dbacadb59b6138dc43f31a603186e790
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-05 13:18:35 -0500

    Update roleCreate.js

--- src/events/roleCreate.js
+++ src/events/roleCreate.js
@@ -1,93 +1,52 @@
-const { AuditLogEvent } = require("discord.js");
 const { createLog } = require("../logger/logger");
 const channels = require("../logger/channels");
-
 
 module.exports = {
     name: "roleCreate",
 
     async execute(role) {
 
-        let executor = null;
-
-
-        try {
-            const logs = await role.guild.fetchAuditLogs({
-                type: AuditLogEvent.RoleCreate,
-                limit: 5
-            });
-
-
-            const entry = logs.entries.find(
-                e => e.target.id === role.id
-            );
-
-
-            if (entry) {
-                executor = entry.executor;
-            }
-
-        } catch {}
-
-
-
         await createLog(role.guild, {
 
-            type: "server",
+            type: "role",
 
             action: "Role Created",
 
-
             target: role.id,
 
-
-            executor: executor
-                ? executor.id
-                : null,
-
+            executor: null,
 
             description:
-                `Role **${role.name}** was created.`,
-
+                `Role created: ${role.name}`,
 
             severity: "normal",
 
-
-            logChannel: channels.server.roles,
-
+            logChannel:
+                channels.roles.create,
 
             metadata: {
 
-                roleId: role.id,
-
                 roleName: role.name,
+
+                roleId: role.id,
 
                 color: role.hexColor
 
             },
 
-
-            color: 0x57f287,
+            color: 0x00ff00,
 
 
             fields: [
 
                 {
-                    name: "Role ID",
-                    value: role.id
+                    name: "Role",
+                    value: `${role.name} (${role.id})`
                 },
 
                 {
                     name: "Position",
-                    value: `${role.position}`,
-                    inline: true
-                },
-
-                {
-                    name: "Created By",
-                    value: executor
-                        ? `${executor.tag} (${executor.id})`
-                        : "Unknown"
+                    value: `${role.position}`
                 }
 
             ]
