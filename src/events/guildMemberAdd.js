92fba294973504aa01776ad734b0b13eba2f24c8
eye-0019 <eye6286@gmail.com>
2026-09-05 22:24:13 -0500

    feat: random welcome messages

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,6 +1,29 @@
 const { createLog } = require("../logger/logger");
 const channels = require("../logger/channels");
 
+const welcomeMessages = [
+    `welcome <@{id}> 👀`,
+    `<@{id}> just joined, hey!`,
+    `we got a new one, welcome <@{id}> 🗣️`,
+    `<@{id}> pulled up, welcome in`,
+    `hey <@{id}>, glad you're here ❤️`,
+    `<@{id}> just joined the server`,
+    `welcome to sɪʟᴇɴᴛ ᴇʏᴇ <@{id}> 😼`,
+    `<@{id}> is here, welcome in`,
+    `no way <@{id}> actually joined 😉`,
+    `welcome <@{id}>, make yourself at home`,
+    `<@{id}> just dropped, welcome in 😌`,
+    `hey <@{id}>, welcome to the server`,
+    `<@{id}> joined, glad to have you 👅`,
+    `welcome in <@{id}>`,
+    `<@{id}> is now a member, welcome! 🤤`,
+    `glad you made it <@{id}>`,
+    `<@{id}> just joined 😼`,
+    `the server just got better, hey <@{id}>`,
+    `welcome <@{id}>, enjoy your stay ❤️`,
+    `<@{id}> joined sɪʟᴇɴᴛ ᴇʏᴇ`,
+];
+
 module.exports = {
     name: "guildMemberAdd",
     async execute(member) {
@@ -21,7 +44,9 @@ module.exports = {
             try {
                 const welcomeChannel = await member.guild.channels.fetch(welcomeChannelId).catch(() => null);
                 if (welcomeChannel?.isTextBased()) {
-                    await welcomeChannel.send(`Welcome to **${member.guild.name}**, <@${member.id}>! 👋`);
+                    const random = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
+                    const message = random.replace(/{id}/g, member.id);
+                    await welcomeChannel.send(message);
                 }
             } catch (err) {
                 console.error('[guildMemberAdd] Failed to send welcome message:', err);
@@ -36,6 +61,7 @@ module.exports = {
             description: `${member.user.tag} joined the server.`,
             severity: "normal",
             logChannel: channels.members.join,
+            thumbnailUrl: member.user.displayAvatarURL({ dynamic: true }),
             metadata: {
                 username: member.user.tag,
                 userId: member.id,
