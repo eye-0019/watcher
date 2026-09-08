236079c68fb45356529592e665baf3fd20706f8b
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-09-07 23:25:07 -0500

    Update setuprules.js

--- src/commands/Admin/setuprules.js
+++ src/commands/Admin/setuprules.js
@@ -20,21 +20,19 @@ module.exports = {
             .setDescription(
                 `─────────────────────────────\n\n` +
                 `⌕ **don't be a loser**\n` +
-                `> jokes and arguing are fine. harassing, bullying, or targeting someone isn't.\n\n` +
+                `> jokes and arguing are fine. harassing, bullying, or targeting someone isn't... only sometimes tho\n\n` +
                 `⌕ **respect boundaries**\n` +
-                `> if someone tells you to stop, stop. no means no, you horny freaks.\n\n` +
+                `> if someone tells you to stop, stop. no means no, you horny freak.\n\n` +
                 `⌕ **keep it legal**\n` +
-                `> no illegal content or encouraging illegal activity. if you're 18 please don't talk to a 15 year old weird.\n\n` +
+                `> no illegal content or encouraging illegal activity. if you're 18 please don't talk to a 15 year old weirdo.\n\n` +
                 `⌕ **for nsfw**\n` +
-                `> nsfw is okay just don't be a loser and ask people for stuff more than once. trust me, people will get horny with u in dms if they wanted to.\n\n` +
+                `> nsfw is okay just don't be a loser and ask people for stuff. trust me, people will get horny with u in dms if they wanted to.\n\n` +
                 `⌕ **no doxxing**\n` +
                 `> don't share anyone's private information without their permission.\n\n` +
                 `⌕ **use common sense**\n` +
                 `> don't spam, advertise without permission, or intentionally ruin the server for other people.\n\n` +
                 `⌕ **staff decisions are final**\n` +
                 `> if a mod asks you to stop, listen. if you have an issue, talk to them nicely.\n\n` +
-                `⌕ **only slurs you can claim**\n` +
-                `> you don't want accusations now do you?\n\n` +
                 `─────────────────────────────\n` +
                 `*warnings → timeouts → ban. simple.*`
             )
