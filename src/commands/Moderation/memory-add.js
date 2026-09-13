0d1bb7a952680ff717d34adadb4782bed5357d7f
eye-0019 <eye6286@gmail.com>
2026-09-12 23:08:22 -0500

    upgrade memory add categories

--- src/commands/Moderation/memory-add.js
+++ src/commands/Moderation/memory-add.js
@@ -1,20 +1,47 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { saveNotes } = require('../../utils/aiMemoryStore');
+const { SlashCommandBuilder } = require('discord.js');
+const { updateMemory } = require('../../utils/aiMemoryStore');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('memory-add')
-        .setDescription('Add a manual memory note for a user')
+        .setDescription('Add a categorized memory for a user')
+
         .addUserOption(option =>
             option
                 .setName('user')
                 .setDescription('User to save memory for')
                 .setRequired(true)
         )
+
         .addStringOption(option =>
             option
-                .setName('note')
-                .setDescription('Memory note to save')
+                .setName('category')
+                .setDescription('Memory category')
+                .setRequired(true)
+                .addChoices(
+                    {
+                        name: 'Profile',
+                        value: 'profile'
+                    },
+                    {
+                        name: 'Personality',
+                        value: 'personality'
+                    },
+                    {
+                        name: 'Projects',
+                        value: 'projects'
+                    },
+                    {
+                        name: 'Importance',
+                        value: 'importance'
+                    }
+                )
+        )
+
+        .addStringOption(option =>
+            option
+                .setName('text')
+                .setDescription('Memory to save')
                 .setRequired(true)
         ),
 
@@ -28,17 +55,61 @@ module.exports = {
             });
         }
 
-        const user = interaction.options.getUser('user');
-        const note = interaction.options.getString('note');
 
-        await saveNotes(
+        const user = interaction.options.getUser('user');
+        const category = interaction.options.getString('category');
+        const text = interaction.options.getString('text');
+
+
+        let memoryData = {
+            profile: {},
+            personality: {},
+            projects: {},
+            importance: 0
+        };
+
+
+        switch (category) {
+
+            case 'profile':
+                memoryData.profile = {
+                    note: text
+                };
+                break;
+
+
+            case 'personality':
+                memoryData.personality = {
+                    note: text
+                };
+                break;
+
+
+            case 'projects':
+                memoryData.projects = {
+                    note: text
+                };
+                break;
+
+
+            case 'importance':
+                memoryData.importance = 5;
+                memoryData.profile = {
+                    important_note: text
+                };
+                break;
+        }
+
+
+        await updateMemory(
             user.id,
-            `Manual note: ${note}`
+            memoryData
         );
+
 
         await interaction.reply({
             content:
-                `🧠 Saved memory for **${user.username}**:\n> ${note}`,
+                `🧠 Saved **${category}** memory for **${user.username}**:\n> ${text}`,
             flags: 64
         });
     }
